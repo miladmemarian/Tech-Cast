c10959411fb116951a246721ba00ce94fc65c629
miladmemarian <milad.memarian@gmail.com>
2018-05-02 14:13:52 -0700

    API data fetched.#11

--- tech-cast/server.js
+++ tech-cast/server.js
@@ -1,6 +1,7 @@
 require('dotenv/config')
 const express = require('express')
 const { MongoClient } = require('mongodb')
+const fetch = require('node-fetch')
 
 const app = express()
 
@@ -26,3 +27,14 @@ MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
     console.log('Listening on 3000')
   })
 })
+
+fetch(
+  'https://listennotes.p.mashape.com/api/v1/search?genre_ids=131&len_max=10&len_min=2&offset=0&published_after=1390190241000&published_before=1490190241000&q=Tech&sort_by_date=0&type=episode',
+  {
+    headers: {
+      'X-Mashape-Key': 'mngC4WsvycmshhTdeKW6AgMvKTMCp1rk63IjsnLBrOvDfMMWF4'
+    }
+  }
+)
+  .then(res => res.json())
+  .then(json => console.log(json))
