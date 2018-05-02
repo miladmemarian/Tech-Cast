ac619a6393a67ede80171ace5299df37d71a9c30
miladmemarian <milad.memarian@gmail.com>
2018-05-02 14:21:29 -0700

    Express route updated.#11

--- tech-cast/server.js
+++ tech-cast/server.js
@@ -1,40 +1,23 @@
 require('dotenv/config')
 const express = require('express')
-const { MongoClient } = require('mongodb')
 const fetch = require('node-fetch')
 
 const app = express()
 
-MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-
-  const db = client.db('tech-cast')
-  const podcasts = db.collection('podcasts')
-
-  app.get('/', (req, res) => {
-    podcasts
-      .find()
-      .toArray()
-      .then(podcasts => res.send(podcasts))
-      .catch(() => res.sendStatus(500))
-  })
-
-  app.use(express.static('images'))
-  app.listen(process.env.PORT, () => {
-    console.log('Listening on 3000')
-  })
+app.get('/', (req, res) => {
+  fetch(
+    'https://listennotes.p.mashape.com/api/v1/search?genre_ids=131&len_max=10&len_min=2&offset=0&published_after=1390190241000&published_before=1490190241000&q=Tech&sort_by_date=0&type=episode',
+    {
+      headers: {
+        'X-Mashape-Key': 'mngC4WsvycmshhTdeKW6AgMvKTMCp1rk63IjsnLBrOvDfMMWF4'
+      }
+    }
+  )
+    .then(podcasts => podcasts.json())
+    .then(podcasts => res.send(podcasts))
+    .catch(() => res.sendStatus(500))
 })
 
-fetch(
-  'https://listennotes.p.mashape.com/api/v1/search?genre_ids=131&len_max=10&len_min=2&offset=0&published_after=1390190241000&published_before=1490190241000&q=Tech&sort_by_date=0&type=episode',
-  {
-    headers: {
-      'X-Mashape-Key': 'mngC4WsvycmshhTdeKW6AgMvKTMCp1rk63IjsnLBrOvDfMMWF4'
-    }
-  }
-)
-  .then(res => res.json())
-  .then(json => console.log(json))
+app.listen(process.env.PORT, () => {
+  console.log('Listening on 3000')
+})
